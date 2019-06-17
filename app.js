d07717faf300de707be69ad9c30894b21f7b182a
jcsparks <jeremy@jcsparks.com>
2019-06-17 12:59:20 -0600

    Switching over to inline templates for the web component which in general seems less appealing but I think actually makes more sense for this use-case where the data is being pulled in from a JSON API call anyway.

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-// var jsonURL = "http://localhost/boulder/jsonapi/node/how_to";
 
 Vue.component('ucb-howto', {
     props: {
         dataurl: ''
     },
-    template: '<div class="ucb-component">\n' +
-        '   <div class="ucb-error-check" v-if="error">\n' +
-        '       <h4 class=ucb-vue-error>{{ error }} </h4>\n' +
-        '   </div>\n'  +
-        '   <div class="ucb-vue-component" v-else>\n' +
-        '    <div v-for="howto in posts">\n' +
-        '    <div class="ucb-how-to ucb-panel" v-show="howto.attributes.title">\n' +
-        '        <div class="ucb-howto-title" v-html="howto.attributes.title"></div>\n' +
-        '        <div class="ucb-howto-description" v-html="howto.attributes.body.value"></div>\n' +
-        '        <ol class="ucb-howto-instructions">\n' +
-        '            <li v-for="instruction in howto.attributes.field_ucb_howto_instructions" v-html="instruction.value">\n' +
-        '            </li>\n' +
-        '        </ol>\n' +
-        '\n' +
-        '        <div class="ucb-howto-contact">Need additional assistance : <a :href="`mailto:${howto.attributes.field_ucb_howto_contact}`" >{{ howto.attributes.field_ucb_howto_contact }}</a></div>\n' +
-        '    </div>\n' +
-        '    </div></div></div>',
     data: function() {
         return {
             error: '',
